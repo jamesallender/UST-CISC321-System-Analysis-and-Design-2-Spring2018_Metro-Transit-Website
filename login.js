fc028491a370acd59de8108b224caf38bbd3acd5
James <jamesa52416@gmail.com>
2018-05-08 16:17:29 -0500

    login 2
    login 2

--- login.js
+++ login.js
@@ -6,21 +6,21 @@ function initialize() {
 	input.addEventListener('keyup', function(event) {
     	if (event.keyCode === 13) {
             if(input.value.length<1||input2.value.length<1) {alert('Please enter an ID and Pin number!');}
-    		else {document.location.href = 'menu.html';}
+    		else {document.location.href = 'Menu.html';}
     	}//if (event.keyCode === 13) {
  	});//input.addEventListener('keyup', function(event) {
 
     loginButton.addEventListener('click', function(event) {
         if(input.value.length<1||input2.value.length<1) {alert('Please enter an ID and Pin number!');
         } else {
-            document.location.href = 'menu.html';
+            document.location.href = 'Menu.html';
         }//else
     });//loginButton.addEventListener('click', function(event) {
 
 	input2.addEventListener('keyup', function(event) {    
     if (event.keyCode === 13) {
             if(input.value.length<1||input2.value.length<1) {alert('Please enter an ID and Pin number!');}
-            else {document.location.href = 'menu.html';}
+            else {document.location.href = 'Menu.html';}
         }//if (event.keyCode === 13)
  });//input2.addEventListener('keyup', function(event) {   
 }
